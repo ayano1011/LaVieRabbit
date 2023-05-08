e88edefe50e0e44f62135304b5aaa33a153581e1
shouhei <shohei.nagamura@gmail.com>
2023-05-08 15:09:15 +0900

    日付選択でデータ取得できるように実装

--- src/App.jsx
+++ src/App.jsx
@@ -8,53 +8,55 @@ import { ja } from "date-fns/locale";
 
 function App() {
   const [weight, setWeight] = useState([])
-  const [data,setData] = useState(null)
+  const [data, setData] = useState(null)
+  const [date, setDate] = useState(format(new Date(), "yyyy-MM-dd"));
 
-  const getData = () => {
-		const requestUrl = `/weight/`;
-		axios
-			.get(requestUrl)
+
+  //選択した日付の重さ情報を取得
+  const getWeight = () => {
+    const requestUrl = `weight/selectDate/${date}`;
+    axios
+      .get(requestUrl)
       .then((response) => {
-        // console.log(response.data);
-        setWeight(response.data);
-
         const format_time = response.data.map((item) => {
           return {
-            weight: item.weight,
+            count: item.count,
             time: format(Date.parse(item.created_at), "HH:mm")
           }
         })
         setData(format_time);
         console.log(format_time);
-        // console.log(data);
+        console.log(data);
       })
-
-			.catch((error) => {
-				console.log(error);
-			});
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
+//選択した日付に切り替える
+  const selectDateHandler = (e) => {
+    console.log(e.target.value);
+    setDate(e.target.value);
+  }
+
 
 
 // 初回レンダリング時に実行
   useEffect(() => {
-    getData();
-  }, []);
+    getWeight();
+  }, [date]);
 
 
-  // テストデータ
-//   const data = [
-//   { time: '0:00', count: 10, average: 5 },
-//   { time: '3:00', count: 15, average: 6 },
-//   { time: '6:00', count: 12, average: 4 },
-//   { time: '9:00', count: 18, average: 7 },
-//   { time: '12:00', count: 20, average: 8 },
-//   { time: '15:00', count: 17, average: 6 },
-//   { time: '18:00', count: 14, average: 5 },
-//   { time: '21:00', count: 11, average: 4 }
-// ]
 
   return (
     <>
+      <input type="date" onChange={selectDateHandler}  value={date}/>
+  
+      {/* <div>
+        {formattedDate}
+      </div> */}
+
+
       <div className="total-area">
         <p>合計</p>
         <div className="total-text">
@@ -63,8 +65,6 @@ function App() {
       </div>
 
 
-      {/* <h1>La Vie Rabbit</h1> */}
-      {/* <button onClick={getData}>クリック</button> */}
       <BarChart
         width={600}
         height={400}
@@ -76,7 +76,7 @@ function App() {
       <YAxis />
       <Tooltip />
       <Legend />
-      <Bar dataKey="weight" name='個数' fill="#8884d8" />
+      <Bar dataKey="count" name='個数' fill="#8884d8" />
       <Bar dataKey="average" name='平均' fill="#82ca9d" />
     </BarChart>
     </>
