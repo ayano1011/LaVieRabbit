506ae6ad1df38c66398f81174a7aea077d9d1080
shouhei <shohei.nagamura@gmail.com>
2023-05-09 20:36:44 +0900

    グラフの表示をreact-chart-jsに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState ,useEffect} from 'react'
 import './App.css'
 import axios from '@/lib/axios'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import { formatDistance, format } from "date-fns";
 import { ja } from "date-fns/locale";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, TimeScale, Tooltip, Legend, PointElement, LineElement } from 'chart.js';
+import { Line,Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+import 'chartjs-adapter-date-fns';
 
 
 function App() {
-  const [weight, setWeight] = useState([])
+  // const [weight, setWeight] = useState([])
   const [data, setData] = useState(null)
   const [date, setDate] = useState(format(new Date(), "yyyy-MM-dd"));
-
+  // const [labels, setLabels] = useState([1,21]);
+  // const [weightData, setWeightData] = useState([1, 2]);
+  const [isLorded, setIsLorded] = useState(false);
 
   //選択した日付の重さ情報を取得
   const getWeight = () => {
@@ -24,9 +29,22 @@ function App() {
             time: format(Date.parse(item.created_at), "HH:mm")
           }
         })
-        setData(format_time);
-        console.log(format_time);
-        console.log(data);
+        const labels = format_time.map((obj) => obj.time)
+        const weightData = format_time.map((obj) => obj.count);
+        
+        
+        setData({
+          labels,
+          datasets: [
+            {
+              label: "個数",
+              data: weightData,
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+          ],
+        })
+
+        setIsLorded(true);
       })
       .catch((error) => {
         console.log(error);
@@ -39,6 +57,49 @@ function App() {
     setDate(e.target.value);
   }
 
+  ChartJS.register(
+		CategoryScale,
+		LinearScale,
+		BarElement,
+		Title,
+		TimeScale,
+		Tooltip,
+		Legend,
+		PointElement,
+		LineElement
+  );
+  
+  const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				// display: false,
+			},
+			title: {
+				display: true,
+				text: "Chart.js Bar Chart",
+			},
+		},
+		scales: {
+			x: {
+				type: "time",
+				min: "00:00",
+				max: "23:59",
+				title: {
+					display: true,
+					text: "時間",
+				},
+        time: {
+          parser:"HH:mm",
+					// unit: "hour",
+				},
+				ticks: {
+					stepSize: 3,
+				},
+			},
+		},
+  };
+
 
 
 // 初回レンダリング時に実行
@@ -48,10 +109,11 @@ function App() {
 
 
 
+
   return (
     <>
       <input type="date" onChange={selectDateHandler}  value={date}/>
-  
+
       {/* <div>
         {formattedDate}
       </div> */}
@@ -65,20 +127,26 @@ function App() {
       </div>
 
 
-      <BarChart
+      {/* <BarChart
         width={600}
         height={400}
         data={data}
         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
       >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="time" />
+      <XAxis dataKey="time" type='time' scale='time' interval='hour'/>
       <YAxis />
       <Tooltip />
       <Legend />
       <Bar dataKey="count" name='個数' fill="#8884d8" />
       <Bar dataKey="average" name='平均' fill="#82ca9d" />
-    </BarChart>
+    </BarChart> */}
+
+
+
+      {isLorded ? <Bar options={options} data={data} /> : <p>Loading...</p>}
+
+      
     </>
   )
 }
